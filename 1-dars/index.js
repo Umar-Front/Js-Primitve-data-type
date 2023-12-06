58d3507a6aa0631fca82254fcb0ec4c8405f2ba8
Umar-Front <khashimovumarali@gmail.com>
2023-12-06 15:05:30 +0500

    lesson-1

---
+++ 1-dars/index.js
@@ -0,0 +1,92 @@
+// let name = "ilya"
+// console.log(`Hello ${1}`);
+// console.log(`Hello ${"name"}`);
+// console.log(`Hello ${name}`);
+
+
+// console.log("" + 1 + 0);
+
+// console.log("" - 1 + 0);
+
+// console.log(true + false);
+// console.log(6 / "3");
+// console.log("2" * "3");
+// console.log(4 + 5 + "px");
+// console.log("$" + 4 + 5);
+// console.log("4" - 2);
+// console.log("4px" - 2);
+// console.log("-9" + 5);
+// console.log(null + 1);
+// console.log(undefined + 1);
+// console.log("\t \n" - 2);
+
+// let name = "ITALY";
+// alert(` hello ${1} `);
+// // hello 1;
+// alert(` hello ${"name"}`)
+// // hello name
+
+// alert(`hello ${name}`);
+// Hello ITALY
+
+
+// let a = 1, b = 1;
+
+// let c = ++a;
+// let d = b++;
+// console.log(c, d);
+// natija = c = 2, d = 1 ; c = 2 sababi ++a malumki incredent avval kelsa oldin 1 son qo'shilib keyin consolega natija chiqadi , b = 1 ++ ,incrediment b dan keyin qo'yilganligi uchun console oldin b qiymatini chiqarib +1 birni xotirada saqlab turadi
+
+// let a = 2;
+// let x = 1 + (a *= 2);
+// console.log(x)
+
+// console.log("" + 1 + 0);
+
+// //natija = 1 0 ; string + belgisini ikki sonni qoshish emas shunchaki ketma - ket kelgan malumot deb xisobladi
+
+
+// console.log("" - 1 + 0);
+// // natija = - 1 ; stringdan keyin - ishorasi borligi sababli xisoblab chiqardi
+
+// console.log(true + false);
+// // natija = true ; true = 1, false = 0; 1 + 0 = 1
+
+// console.log(6 / "3");
+// // natija = 2; stringdan oldingi belgi + bo'lmaganligi uchun xisobladi
+// console.log("2" * "3");
+// //  natija = 6; bunda ham shu xolat takrorlangan
+// console.log(4 + 5 + "px");
+// // natija = 9 px; stringni ichida son dan boshqa belgi borligi uchun yeg'indini oldiga  px chiqardi
+// console.log("4" - 2);
+
+// // natija = 2; bu yerda ham yuqoridagilar kabi - ishorasi sabab xisoblab berdi
+// console.log("4px" - 2);
+// // naiaja = NaN; strinda sondan boshqa belgi borligi uchun buni raqam deb qabul qimadi
+// console.log("$" + 4 + 5);
+// // natija = $ 4 5; stringdan keyingi + sabab
+// console.log("  -9 " + 5);
+// //natija = -9 5 ; stringdan keyin + borligi uchun
+// console.log(null + 1);
+// // natija = 1 ; null = 0 , 0 + 1 = 1
+// console.log(undefined + 1);
+// // undefined qiymat nomalum bo'lganligi uchun
+
+// console.log("/t /n" - 2);
+// // natija = NaN ; stringni ichida sondan boshqa belgi borligi uchun
+
+// console.log(5 > 4);
+// // natija = true; tabiki 5 katta taqqoslash belgisi < > katta kichiklikga ishlatiladi
+// console.log("apple" > "pineapple")
+// //natija = false; apple so'z uzunligi bo'yicha qisqa
+
+// console.log(undefined == null);
+// // natija = false ; 0 = 0 degandek
+
+// console.log(null == "\n0\n");
+
+
+// if ("0") {
+//   console.log('Hello');
+// }
+// natija = Hello => string ichida ma'lumot bo
